58f723726ec1f5f41d4b98f05302c5eb0fc7a858
Orie Steele <orie@transmute.industries>
2018-04-22 16:53:39 -0500

    testing

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,5 @@ RUN apk add -U gpgme
 
 WORKDIR /usr/src/app
 COPY . .
+
+RUN npm i
--- setup.sh
+++ setup.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
 # Create a Key
-gpg --batch --yes --no-tty --gen-key ./docs/alice.eddsa
-echo 'hello' `date +%s` > ./docs/doc
+gpg --batch --yes --no-tty --gen-key ./gen-alice.eddsa
+echo 'hello' `date +%s` > ./data/doc
 
 # Encrypt & Decrypt
-echo 'secret' | gpg --batch --passphrase-fd 0 --no-tty -c ./docs/doc
-echo 'secret' | gpg --batch --passphrase-fd 0 --no-tty -d ./docs/doc.gpg 
+echo 'secret' | gpg --batch --passphrase-fd 0 --no-tty -c ./data/doc
+echo 'secret' | gpg --batch --passphrase-fd 0 --no-tty -d ./data/doc.gpg 
 
 # Sign and Verify
-echo "secret" | PASSPHRASE="secret" gpg --batch --pinentry-mode loopback --command-fd 0 --output ./docs/doc.sig --sign --detach-sig ./docs/doc
-gpg --verify ./docs/doc.sig ./docs/doc 
+echo "secret" | gpg --batch --pinentry-mode loopback --command-fd 0 --output ./data/doc2.sig --sign ./data/doc
+gpg --verify ./data/doc2.sig 
 
 # Export a Public Key
-gpg --export -a "Alice" > ./docs/public.key
+gpg --export -a "Alice" > ./data/public.key
 # cat public.key
 
 # Export a Private Key
-echo "secret" | PASSPHRASE="secret" gpg --batch --pinentry-mode loopback --command-fd 0 --export-secret-key -a "Alice" > ./docs/private.key
+echo "secret" | gpg --batch --pinentry-mode loopback --command-fd 0 --export-secret-key -a "Alice" > ./data/private.key
 # cat private.key
+
+# Export All
+gpg -a --export > ./data/public_keys.asc
+echo "secret" | gpg --batch --pinentry-mode loopback --command-fd 0 -a --export-secret-keys > ./data/private_keys.asc
+gpg --export-ownertrust > ./data/trust.txt
+
+npm run test:ci
